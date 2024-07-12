6a950f7dbf0af182e7744c94235f554649c81741
mukhlisamakhmaraimova <makhmaraimovamukhlisa77@gmail.com>
2024-07-12 23:38:04 +0500

    myproject

--- news/main/urls.py
+++ news/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home,about, contact
+from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
     path('about', about, name='about'), #yangilik.uz/about
-    path('contact', contact, name='contact')
+    path('contact', contact, name='contact'),
+    path('category', category, name='category'),
+    path('search-result', search, name='search'),
+    path('single-post', singlepost, name='single')
 ]
--- news/main/views.py
+++ news/main/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Yangilik
 # Create your views here.
 
 def home(request):
+    yangiliklar = Yangilik.objects.all()
 
-    return render(request, 'main/home.html')
+    context = {
+        'news': yangiliklar
+    }
+
+    return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')
 
 def contact(request):
     return render(request, 'main/contact.html')
+
+def category(request):
+    return render(request, 'main/category.html')
+
+def search(request):
+    return render(request, 'main/search-resulthtml')
+
+def singlepost(request):
+    return render(request, 'main/single-post.html')
